def483e487359b1556d3ac87b4e73ec39952a30b
RitaM5 <sporshia108514orva@gmail.com>
2023-05-19 02:27:24 +0600

    gallery section added

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+const Gallery = ({ gallery }) => {
+    const { image } = gallery;
+    return (
+        <>
+            <div className="card card-compact w-full py-9 bg-base-100 shadow-xl hover:-translate-y-7 transition ease-in-out delay-250">
+                <LazyLoadImage
+                className='w-72 h-72' src={image} alt="" 
+                effect="blur"
+                />
+               
+            </div>
+        </>
+            
+    );
+};
+
+export default Gallery;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Banner from '../../Shared/Banner/Banner';
+import Gallery from '../Gallery/Gallery';
 
 const Home = () => {
+    const galleries = [
+        { id: 1, image: "https://i.ibb.co/Tgy6tCt/princess-1.jpg" },
+        { id: 2, image: "https://i.ibb.co/jZqftRS/princess-2.jpg" },
+        { id: 3, image: "https://i.ibb.co/djZT1CN/princess-7.jpg" },
+        { id: 4, image: "https://i.ibb.co/CJz5zhh/princess-4.jpg" },
+        { id: 5, image: "https://i.ibb.co/Y7C83t1/frozen-1.jpg" },
+        { id: 6, image: "https://i.ibb.co/7YPLVQb/frozen-7.jpg" },
+        { id: 7, image: "https://i.ibb.co/LP9frHz/frozen-3.jpg" },
+        { id: 8, image: "https://i.ibb.co/rZZ7TP8/frozen-6.jpg" },
+        { id: 9, image: "https://i.ibb.co/zb7thCN/ani-1.jpg" },
+        { id: 10, image: "https://i.ibb.co/D4PjNJX/ani-2.jpg" },
+        { id: 11, image: "https://i.ibb.co/FzR4x0j/ani-3.jpg" },
+        { id: 12, image: "https://i.ibb.co/6yKynDn/ani-4.jpg" },
+    ]
     return (
         <div>
             <Banner></Banner>
+            <div className='my-container'>
+                <h1 className='underline underline-offset-4 font-semibold text-2xl text-center'>Gallery</h1>
+                <h2 className='mt-4 text-lg text-center'>We make your children happier with the best toys</h2>
+                <div className='grid grid-cols-3 gap-5 my-9'>
+                    {
+                        galleries.map(gallery => <Gallery key={gallery.id} gallery={gallery}></Gallery>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
