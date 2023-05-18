a252a3ff6758f3c3aac063e9f82bc51fac4f767e
RitaM5 <sporshia108514orva@gmail.com>
2023-05-19 02:49:42 +0600

    aos animation installed

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -5,15 +5,15 @@ const Gallery = ({ gallery }) => {
     const { image } = gallery;
     return (
         <>
-            <div className="card card-compact w-full py-9 bg-base-100 shadow-xl hover:-translate-y-7 transition ease-in-out delay-250">
+            <div data-aos="fade-up"
+                data-aos-duration="3000" className="card card-compact w-full py-9 bg-base-100 shadow-xl hover:-translate-y-7 transition ease-in-out delay-250">
                 <LazyLoadImage
-                className='w-72 h-72' src={image} alt="" 
-                effect="blur"
+                    className='w-72 h-72 mx-auto' src={image} alt=""
+                    effect="blur"
                 />
-               
             </div>
         </>
-            
+
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import router from './Routes/Routes'
 import AuthProvider from './Providers/AuthProvider.jsx'
 import { RouterProvider } from 'react-router-dom'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
